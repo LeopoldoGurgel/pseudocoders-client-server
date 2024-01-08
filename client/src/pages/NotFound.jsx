5d1f702dd333ee54ed86a1c2f5a25cc3af69d5a6
LeopoldoGurgel <leopoldogbp@gmail.com>
2024-01-08 14:53:36 -0500

    Updates

--- client/src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-// create a page not found page
-
-export default function NotFound(){
-    return(
-        <div>
-            <h2>Page under construction</h2>
-        </div>
-    )
-}
--- server/models/index.js
+++
@@ -1,6 +0,0 @@
-const User = require("./User");
-const Post = require("./Post.js");
-const Comment = require("./Comment");
-const VerificationToken = require('./VerificationToken');
-
-module.exports = {User, Post, Comment, VerificationToken};
