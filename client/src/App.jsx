41a9487ba3fc0f6c50129c7185e7e19effbd189e
LeopoldoGurgel <leopoldogbp@gmail.com>
2024-01-10 11:21:37 -0500

    changed server uri

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Container from './components/Container';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: 'https://pseudocoders-server-71715e6b7ea8.herokuapp.com/graphql',
+  uri: 'https://pseudocoders-server-c1afe736ec03.herokuapp.com/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
